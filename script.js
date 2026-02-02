3f395267c91bc743a3e465d108ef8d8616bba4e9
Kajuss-bots <keturkakajuss@gmail.com>
2026-02-02 17:05:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const bots = [
+  {
+    name: "Crazy Lithuanians",
+    description: "Tickets/level/linkrsn/monthly clan xp",
+    avatar: "https://cdn-icons-png.flaticon.com/512/4712/4712109.png",
+    invite: "https://discord.com/oauth2/authorize?client_id=1440312480395169793&permissions=8&integration_type=0&scope=bot",
+    support: "https://discord.gg/CMqTmZdMfN"
+  },
+  {
+    name: "Latitane Bot",
+    description: "Siphoned energy + sell funtioncs.",
+    avatar: "https://cdn-icons-png.flaticon.com/512/4712/4712139.png",
+    invite: "https://discord.com/oauth2/authorize?client_id=1459178434361298974&permissions=0&integration_type=0&scope=bot",
+    support: "https://discord.gg/CMqTmZdMfN"
+  },
+  {
+    name: "SignUp",
+    description: "SignUp for mass. /signup",
+    avatar: "https://cdn-icons-png.flaticon.com/512/4712/4712166.png",
+    invite: "https://discord.com/oauth2/authorize?client_id=1464333078502703169&permissions=8&integration_type=0&scope=bot",
+    support: "https://discord.gg/CMqTmZdMfN"
+  },
+  {
+    name: "Ticket Bot",
+    description: "Bot with Ticket system",
+    avatar: "https://cdn-icons-png.flaticon.com/512/4712/4712166.png",
+    invite: "https://discord.com/oauth2/authorize?client_id=1457309109677326431&permissions=8&integration_type=0&scope=bot",
+    support: "https://discord.gg/CMqTmZdMfN"
+  }
+];
+
+const botsContainer = document.getElementById("bots");
+
+bots.forEach(bot => {
+  const div = document.createElement("div");
+  div.className = "bot";
+
+  div.innerHTML = `
+    <img src="${bot.avatar}" alt="${bot.name}">
+    <h2>${bot.name}</h2>
+    <p>${bot.description}</p>
+    <div class="buttons">
+      <a href="${bot.invite}" target="_blank">Invite</a>
+      <a href="${bot.support}" target="_blank" class="secondary">Support</a>
+    </div>
+  `;
+
+  botsContainer.appendChild(div);
+});
